48a69d539c099dc784f84b6f8b7481c70264d38c
xueli.xue <xueli.xue@dianping.com>
2019-01-10 18:55:57 +0800

    更新RM

---
+++ xxl-apm-client/src/main/java/com/xxl/apm/client/admin/util/HeartbeatUtil.java
@@ -0,0 +1,15 @@
+package com.xxl.apm.client.admin.util;
+
+import com.xxl.apm.client.admin.param.XxlApmHeartbeat;
+
+/**
+ * @author xuxueli 2019-01-10
+ */
+public class HeartbeatUtil {
+
+    public static XxlApmHeartbeat generateHeartbeat(){
+
+        return null;
+    }
+
+}
